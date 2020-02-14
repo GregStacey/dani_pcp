0281f3c30760313c768932233cdd8a5dafe872a3
Greg Stacey <gregstacey@dhcp-206-87-97-111.ubcvisitor.wireless.ubc.ca>
2020-02-13 16:29:18 -0800

    New version

--- R/plot_prince.R
+++ R/plot_prince.R
@@ -1,33 +1,40 @@
 
+source("functions_dani.R") # ml.ints, hl.ints
+ml.ints = ml.ints[ml.ints$precision >= 0.5 & !is.na(ml.ints$precision), ]
+hl.ints = hl.ints[hl.ints$precision >= 0.5 & !is.na(hl.ints$precision), ]
+
 # plot chromatograms
-interactions.hl$unique.interaction = character(nrow(interactions.hl))
-for (ii in 1:nrow(interactions.hl)) {
-  interactions.hl$unique.interaction[ii] = paste(sort(interactions.hl[ii,1:2]), collapse="_")
+hl.ints$unique.interaction = character(nrow(hl.ints))
+for (ii in 1:nrow(hl.ints)) {
+  hl.ints$unique.interaction[ii] = paste(sort(hl.ints[ii,1:2]), collapse="_")
 }
 
-interactions.ml$unique.interaction = character(nrow(interactions.ml))
-for (ii in 1:nrow(interactions.ml)) {
-  interactions.ml$unique.interaction[ii] = paste(sort(interactions.ml[ii,1:2]), collapse="_")
+ml.ints$unique.interaction = character(nrow(ml.ints))
+for (ii in 1:nrow(ml.ints)) {
+  ml.ints$unique.interaction[ii] = paste(sort(ml.ints[ii,1:2]), collapse="_")
 }
 
-interactions = full_join(interactions.hl, interactions.ml, by="unique.interaction")
+# order by max precision
+interactions = full_join(hl.ints, ml.ints, by="unique.interaction")
+tmp = cbind(interactions$precision.x,interactions$precision.y)
+I = order(apply(tmp, 1, max, na.rm=T), decreasing = T)
+interactions = interactions[I,]
 
 # interactions in both
 xx = 0.5
 I = which(interactions$precision.x>xx & interactions$precision.y>xx)
 
 # interactions in just ML
-I1 = which(interactions$precision.x>xx & interactions$precision.y<xx)
-I2 = which(interactions$precision.x<xx & interactions$precision.y>xx)
+I1 = which(interactions$precision.x>xx & is.na(interactions$precision.y))
+I2 = which(interactions$precision.y>xx & is.na(interactions$precision.x))
 labels = c("M+H interaction", "Only M interaction", "Only H interaction")
 
 I2plot = c(I, I1, I2)
-for (ii in 1:length(I2plot)) {
-  protA = interactions$protein_A.x[I2plot[ii]]
-  protB = interactions$protein_B.x[I2plot[ii]]
-  
-  ia = which(rownames(dani[[1]]) == protA)
-  ib = which(rownames(dani[[1]]) == protB)
+conds = c("M/L", "H/L")
+for (ii in 8548:length(I2plot)) {
+  prots = unlist(strsplit(interactions$unique.interaction[I2plot[ii]], "_"))
+  protA = prots[1]
+  protB = prots[2]
   
   if (I2plot[ii] %in% I) {
     label = labels[1]
@@ -37,20 +44,35 @@ for (ii in 1:length(I2plot)) {
     label = labels[3]
   }
   
-  df = data.frame(protA.ml = dani[[1]][ia,],
-                  protB.ml = dani[[1]][ib,],
-                  protA.hl = dani[[2]][ia,],
-                  protB.hl = dani[[2]][ib,], stringsAsFactors = F)
-  df$fraction = as.numeric(sapply(sapply(as.character(rownames(df)), strsplit, ".", fixed=T), "[", 2))
-  df = melt(df, id.vars = "fraction")
+  df = data.frame(protA = numeric(1e4), 
+                  protB = numeric(1e4),
+                  channel = numeric(1e4),
+                  replicate = numeric(1e4),
+                  fraction = numeric(1e4),stringsAsFactors = F)
+  cc = 0
+  for (jj in 1:length(data)) { #channel
+    for (kk in 1:length(data[[jj]])) { #replicate
+      ia = which(rownames(data[[jj]][[kk]]) == protA)
+      ib = which(rownames(data[[jj]][[kk]]) == protB)
+      i0 = (cc+1) : (cc+60)
+      df$fraction[i0] = 1:60
+      df$protA[i0] = data[[jj]][[kk]][ia,]
+      df$protB[i0] = data[[jj]][[kk]][ib,]
+      df$channel[i0] = conds[jj]
+      df$replicate[i0] = kk
+      cc = cc+length(i0)
+    }
+  }
+  df = df[1:cc,]
+  df = melt(df, measure.vars = c("protA", "protB"), 
+            id.vars = c("protA", "protB", "replicate", "channel", "fraction"))
   df$variable = as.character(df$variable)
-  df$channel = sapply(sapply(df$variable, strsplit, ".", fixed=T), "[", 2)
-  df$prot = sapply(sapply(df$variable, strsplit, ".", fixed=T), "[", 1)
-  df$prot[df$prot=="protA"] = protA
-  df$prot[df$prot=="protB"] = protB
-  ggplot(df, aes(x=fraction, y=value, color=prot)) + geom_line() + geom_point() +
-    facet_wrap(~channel) + ggtitle(label)
-  sf = paste("/Users/gregstacey/Academics/Foster/LabMembers/Dani/pcp/figures/chromatograms/", 
+  df$variable[df$variable=="protA"] = protA
+  df$variable[df$variable=="protB"] = protB
+  
+  ggplot(df, aes(x=fraction, y=value, color=variable)) + geom_line() + geom_point() +
+    facet_grid(channel~replicate) + ggtitle(label) + theme_bw()
+  sf = paste("/Users/gregstacey/Academics/Foster/LabMembers/Dani/pcp/figures/chromatograms/3reps/", 
              ii, "_", protA, "_", protB, ".png", sep="")
-  ggsave(sf, width=10, height=3.5, dpi = 100)
+  ggsave(sf, width=8, height=3.5, dpi = 100)
 }
--- R/prince_dani.R
+++ R/prince_dani.R
@@ -4,7 +4,11 @@ require(ggplot2)
 require(readr)
 require(PrInCE)
 require(dplyr)
-
+require(tidyr)
+require(magrittr)
+require(progress)
+require(flavin)
+require(ontologyIndex)
 first = dplyr::first
 
 # read command line args
@@ -62,13 +66,22 @@ save(ml.ints, hl.ints, file=sf)
 
 
 
+# write good interaction lists
+fn = "../data/interactions/interactions_reps7_gs1.Rda"
+load(fn)
+sf = "../data/interactions/interactions_ML.txt"
+write_tsv(ml.ints[which(ml.ints$precision>=0.5),], path=sf)
+sf = "../data/interactions/interactions_HL.txt"
+write_tsv(hl.ints[which(hl.ints$precision>=0.5),], path=sf)
 
 
 ################################ Diagnostics
 
-# how many proteins in common b/w gold stanard and data?
+################################ Are TP and TN pairs different?
+
+#how many proteins in common b/w gold stanard and data?
 unqprots.data = unique(c(rownames(data[[1]][[1]]), rownames(data[[1]][[2]]), rownames(data[[1]][[3]])))
-unqprots.gold = unique(unlist(sapply(gold_standard[[2]], strsplit, ";")))
+unqprots.gold = unique(unlist(sapply(gold_standard[[1]], strsplit, ";")))
 n.overlap = length(intersect(unqprots.gold, unqprots.data))
 print(paste(length(unqprots.data),"unique proteins in data"))
 print(paste(length(unqprots.gold),"unique proteins in gold standard"))
@@ -107,7 +120,7 @@ names(n.mat) = c("protA","protB","nn")
 n.mat$protAB = paste(n.mat$protA, n.mat$protB, sep="_")
 
 # TP, TN, or missing
-gs.mat = adjacency_matrix_from_list(gold_standard)
+gs.mat = adjacency_matrix_from_list(gold_standard[[2]])
 a = as.data.frame(t(combn(rownames(tmp), 2)))
 protsA = as.character(a[,1])
 protsB = as.character(a[,2])
@@ -183,34 +196,285 @@ for (uu in 1:2) {
 
 
 ################################ How good are the complexes
+gs = gold_standard[[2]] # mapped
 unqprots.data = unique(c(rownames(data[[1]][[1]]), rownames(data[[1]][[2]]), rownames(data[[1]][[3]])))
-unqprots.gold = unique(unlist(sapply(gold_standard, strsplit, ";")))
+unqprots.gold = unique(unlist(sapply(gs, strsplit, ";")))
 
 nchan = length(data)
 nrep = length(data[[1]])
 
-df.gs = data.frame(prots = character(length(gold_standard)),
-                   nn = numeric(length(gold_standard)),
-                   nn.overlap = numeric(length(gold_standard)),
-                   rrh1 = numeric(length(gold_standard)), 
-                   rrh2 = numeric(length(gold_standard)), 
-                   rrh3 = numeric(length(gold_standard)), 
-                   rrm1 = numeric(length(gold_standard)), 
-                   rrm2 = numeric(length(gold_standard)), 
-                   rrm3 = numeric(length(gold_standard)), 
+df.gs = data.frame(prots = character(length(gs)),
+                   nn = numeric(length(gs)),
+                   nn.overlap = numeric(length(gs)),
+                   rrh1 = rep(NA,length(gs)), 
+                   rrh2 = rep(NA,length(gs)), 
+                   rrh3 = rep(NA,length(gs)), 
+                   rrm1 = rep(NA,length(gs)), 
+                   rrm2 = rep(NA,length(gs)), 
+                   rrm3 = rep(NA,length(gs)), 
                    stringsAsFactors = F)
-for (ii in 1:length(gold_standard)) {
-  prots = gold_standard[ii]
+for (ii in 1:length(gs)) {
+  prots = unlist(gs[ii])
+  df.gs$prots[ii] = paste(prots, collapse=";")
   
-  I.h1 = match(prots, rownames(data[[1]][[1]]))
+  df.gs$nn[ii] = length(prots)
   
-  for (jj in 1:length(prots)) {
-    for (kk in 1:length(prots)) {
+  I.m1 = match(prots, rownames(data[[1]][[1]])) %>% na.omit
+  I.m2 = match(prots, rownames(data[[1]][[2]])) %>% na.omit
+  I.m3 = match(prots, rownames(data[[1]][[3]])) %>% na.omit
+  I.h1 = match(prots, rownames(data[[2]][[1]])) %>% na.omit
+  I.h2 = match(prots, rownames(data[[2]][[2]])) %>% na.omit
+  I.h3 = match(prots, rownames(data[[2]][[3]])) %>% na.omit
+
+  if (length(I.m1)>=2) df.gs$rrm1[ii] = as.data.frame(as.vector(cor(t(data[[1]][[1]][I.m1,]), , use = "p"))) %>% 
+    rename(x = 1) %>% filter(!x==1) %>% unlist %>% mean
+  if (length(I.m2)>=2) df.gs$rrm2[ii] = as.data.frame(as.vector(cor(t(data[[1]][[2]][I.m2,]), , use = "p"))) %>% 
+    rename(x = 1) %>% filter(!x==1) %>% unlist %>% mean
+  if (length(I.m3)>=2) df.gs$rrm3[ii] = as.data.frame(as.vector(cor(t(data[[1]][[3]][I.m3,]), , use = "p"))) %>% 
+    rename(x = 1) %>% filter(!x==1) %>% unlist %>% mean
+  if (length(I.h1)>=2) df.gs$rrh1[ii] = as.data.frame(as.vector(cor(t(data[[2]][[1]][I.h1,]), , use = "p"))) %>% 
+    rename(x = 1) %>% filter(!x==1) %>% unlist %>% mean
+  if (length(I.h2)>=2) df.gs$rrh2[ii] = as.data.frame(as.vector(cor(t(data[[2]][[2]][I.h2,]), , use = "p"))) %>% 
+    rename(x = 1) %>% filter(!x==1) %>% unlist %>% mean
+  if (length(I.h3)>=2) df.gs$rrh3[ii] = as.data.frame(as.vector(cor(t(data[[2]][[3]][I.h3,]), , use = "p"))) %>% 
+    rename(x = 1) %>% filter(!x==1) %>% unlist %>% mean
+}
+
+I = df.gs$nn>3
+cor(df.gs[I,4:9], use = "p", method = 'spearman')
+
+
+################################ 
+fns = dir("../data/interactions/", full.names = T)
+nn = 15000 
+df = data.frame(rank = numeric(nn* 14 * 2),
+                score = numeric(nn* 14 * 2),
+                precision = numeric(nn* 14 * 2),
+                channel = character(nn* 14 * 2),
+                protA = numeric(nn* 14 * 2),
+                protB = numeric(nn* 14 * 2),
+                gs = numeric(nn* 14 * 2),
+                rep = numeric(nn* 14 * 2), stringsAsFactors = F)
+cc = 0
+for (ii in 1:length(fns)) {
+  load(fns[ii])
+  
+  gs = as.numeric(gsub("gs","", gsub(".Rda","", unlist(strsplit(basename(fns[ii]), "_"))[3])))
+  rep = as.numeric(gsub("reps","", gsub(".Rda","", unlist(strsplit(basename(fns[ii]), "_"))[2])))
+  rep = paste(reps[[rep]], collapse=",")
+  
+  tmp = list(ml.ints, hl.ints)
+  conds = c("ml", "hl")
+  for (jj in 1:length(tmp)) {
+    I = (cc+1) : (cc+nn)
+    df$rank[I] = 1:nn
+    df$score[I] = tmp[[jj]]$score[1:nn]
+    df$precision[I] = tmp[[jj]]$precision[1:nn]
+    df$channel[I] = conds[jj]
+    df$protA[I] = tmp[[jj]]$protein_A[1:nn]
+    df$protB[I] = tmp[[jj]]$protein_B[1:nn]
+    df$gs[I] = gs
+    df$rep[I] = rep
+    cc = cc+nn
+  }
+  
+  ia = which(ml.ints$precision>0.5 | is.na(ml.ints$precision))
+  ib = which(hl.ints$precision>0.5 | is.na(hl.ints$precision))
+  print(paste(ii, ": M/L interactions=", 
+              length(ia),
+              ",  H/L interactions=",
+              length(ib), 
+              ",  total interactions=",length(ia)+length(ib), sep=""))
+}
+
+ggplot(df, aes(x=rank, y=precision, color = channel)) + 
+  facet_grid(gs ~ rep) + 
+  geom_line()
+ggsave("../figures/interaction_scan.png", width = 12, height = 3)
+
+
+
+################################ 
+# do functional differential analysis on best-performing interactions
+# reps = c(1,2,3), gs = un-mapped
+# THIS IS NOW DEPRECATED
+#   needs to be made compatible with differential-explore.R
+
+#source("/differential.R")
+#source("/Users/gregstacey/Academics/Foster/PCP-SILAC/differential/R/differential.R")
+
+# get interactions
+fn = "../data/interactions/interactions_reps7_gs1.Rda"
+load(fn)
+
+# # filter to 50% precision
+# I = which(hl.ints$precision>=0.5)
+# nn = I[length(I)]
+# hl.ints = hl.ints[1:nn, ]
+# I = which(ml.ints$precision>=0.5)
+# nn = I[length(I)]
+# ml.ints = ml.ints[1:nn, ]
+# 
+# # fill in precision=NA with precision=1
+# I = which(!is.na(hl.ints$precision))[1]
+# hl.ints$precision[1:(I-1)] = 1
+# I = which(!is.na(ml.ints$precision))[1]
+# ml.ints$precision[1:(I-1)] = 1
+
+# ERROR
+# For some reason, the first few thousand interactions look bad.
+# Therefore, only use interactions with !is.na() precision.
+ml.ints = ml.ints[ml.ints$precision>=0.5 & !is.na(ml.ints$precision), ]
+hl.ints = hl.ints[hl.ints$precision>=0.5 & !is.na(hl.ints$precision), ]
+
+# run functional differential
+dfsig = differential(ml.ints, hl.ints)
+
+# write functional differential analysis
+nn = sum(!is.na(dfsig$difference.zscore))
+I = order(dfsig$difference.zscore, decreasing = T)
+fn = "../data/functional_differential.txt"
+write_tsv(dfsig[I[1:nn], ], path=fn)
+
+# attach interactions, proteins, and GO IDs to "../data/functional_differential.txt"
+unqprots = "S4R2S6"
+for (ii in 1:2) {
+  for (jj in 1:3) {
+    unqprots = unique(c(unqprots,  rownames(data[[ii]][[jj]])))
+  }
+}
+dfsig = as.data.frame(read_tsv("../data/dani-differential.txt"))
+ia = match(dfsig$go.name, ontology$name)
+dfsig$go.id = ontology$id[ia]
+dfsig$proteins = sapply(dfsig$go.id, FUN=function(x) {
+  paste(unlist(ann[[x]][ann[[x]] %in% unqprots]), collapse = " ")
+})
+dfsig$interactions.hl = character(nrow(dfsig))
+dfsig$interactions.ml = character(nrow(dfsig))
+for (ii in 1:nrow(dfsig)) {
+  prots = unlist(strsplit(dfsig$proteins[ii], " "))
+  I.ml = which(ml.ints$protein_A %in% prots & ml.ints$protein_B %in% prots & !is.na(ml.ints$precision))
+  I.hl = which(hl.ints$protein_A %in% prots & hl.ints$protein_B %in% prots & !is.na(ml.ints$precision))
+  dfsig$interactions.ml[ii] = paste(paste(ml.ints$protein_A[I.ml], 
+                                          ml.ints$protein_B[I.ml], sep="-"), collapse = " ; ")
+  dfsig$interactions.hl[ii] = paste(paste(hl.ints$protein_A[I.hl], 
+                                          hl.ints$protein_B[I.hl], sep="-"), collapse = " ; ")
+}
+write_tsv(dfsig, path="../data/dani-differential-2.txt")
+
+
+
+################################ 
+# plot OGT interactions
+ogt = "Q8CGY8"
+ia = which(ml.ints$precision>=0.5 &
+             (ml.ints$protein_A==ogt | ml.ints$protein_B==ogt))
+ib = which(hl.ints$precision>=0.5 &
+             (hl.ints$protein_A==ogt | hl.ints$protein_B==ogt))
+# no OGT interactions...
+df = data.frame(fraction = numeric(10^4),
+                value = numeric(10^4),
+                condition  =character(10^4),
+                rep = numeric(10^4), stringsAsFactors = F)
+df2plot = data.frame(fraction = numeric(10^4),
+                     value = numeric(10^4),
+                     condition  =character(10^4),
+                     protein = character(10^4),
+                     rep = numeric(10^4), 
+                     plot.number = numeric(10^4),stringsAsFactors = F)
+text.layer = data.frame(x = numeric(10^4), y = numeric(10^4),
+                        label = character(10^4), 
+                        plot.number = numeric(10^4), stringsAsFactors = F)
+df.ogt = data.frame(proteinA = character(1000),
+                    proteinB = character(1000),
+                    correlation = numeric(1000), 
+                    condition = character(1000),stringsAsFactors = F)
+cc = 0
+cc2 = 0
+cc3 = 0
+cc4 = 0
+for (ii in 1:2) { # conditions
+  for (jj in 1:3) { # reps
+    tmp = data[[ii]][[jj]]
+    ia = which(rownames(tmp)==ogt)
+    
+    I = (cc+1) : (cc+60)
+    df$fraction[I] = 1:60
+    df$value[I] = tmp[ia,]
+    df$condition[I] = ii
+    df$rep[I] = jj
+    cc = cc+60
+
+    cor.mat = cor(t(tmp), use = "p")
+    cor.mat = cor.mat[ia,]
+
+    # n samples
+    nsamples = numeric(nrow(tmp))
+    for (kk in 1:nrow(tmp)) {
+      nsamples[kk] = sum(!is.na(tmp[ia,]) & !is.na(tmp[kk,]))
+    }
+    
+    ib = which(cor.mat>.8 & nsamples>10)
+    for (kk in 1:length(ib)) {
+      if (rownames(tmp)[ib[kk]]==ogt) next
+      cc3 = cc3+1
+      I = (cc2+1) : (cc2+60)
+      df2plot$fraction[I] = 1:60
+      df2plot$value[I] = tmp[ia,]
+      df2plot$condition[I] = ii
+      df2plot$protein[I] = 1#"Q8CGY8"
+      df2plot$rep[I] = jj
+      df2plot$plot.number[I] = cc3
+      cc2 = cc2+60
+      I = (cc2+1) : (cc2+60)
+      df2plot$fraction[I] = 1:60
+      df2plot$value[I] = tmp[ib[kk],]
+      df2plot$condition[I] = ii
+      df2plot$protein[I] = 2#rownames(tmp)[ib[kk]]
+      df2plot$rep[I] = jj
+      df2plot$plot.number[I] = cc3
+      cc2 = cc2+60
       
+      cc4 = cc4+1
+      text.layer$label[cc4] = rownames(tmp)[ib[kk]]
+      text.layer$plot.number[cc4] = cc3
+      cc4 = cc4+1
+      text.layer$label[cc4] = "Q8CGY8"
+      text.layer$plot.number[cc4] = cc3
+      
+      df.ogt$proteinA[cc3] = "Q8CGY8"
+      df.ogt$proteinA[cc3] = rownames(tmp)[ib[kk]]
+      df.ogt$correlation[cc3] = cor.mat[ib[kk]]
+      df.ogt$condition[cc3] = ii
     }
   }
 }
+df = df[1:cc,]
+df.ogt = df.ogt[1:cc3,]
+df2plot = df2plot[1:cc2,]
+I = seq(from=1, to=cc4, by=2)
+text.layer = text.layer[1:cc4,]
+text.layer$x = 40
+text.layer$y[I] = 4#max(df.plot$V1, na.rm=T)*.5
+text.layer$y[I+1] = 2.5#max(df.plot$V1, na.rm=T)*.8
+text.layer$protein = ""
+text.layer$protein[I] = 1
+text.layer$protein[I+1] = 2
 
+ggplot(df2plot, aes(x=fraction, y=value, color=protein)) + facet_wrap(~plot.number) +
+  geom_line(alpha=.3) + geom_point(alpha=.5) + 
+  geom_text(data=text.layer[I,], aes(x=x, y=y, label=label), size=3, color="#00BFC4") +
+  geom_text(data=text.layer[I+1,], aes(x=x, y=y, label=label), size=3, color="red") +
+  theme_bw() + ylab("protein amount") +theme(strip.text.x = element_blank()) + 
+  coord_cartesian(ylim=c(0,5))
+ggsave("../figures/ogt_correlators.png", width=8, height=4.8)
 
-
+# just plot ogt
+df$condition[df$condition==1] = "M/L"
+df$condition[df$condition==2] = "H/L"
+df$rep = as.character(df$rep)
+ggplot(df, aes(x=fraction,y=value,color=rep,group=rep)) + 
+  geom_point(alpha=.5) +geom_line(alpha=.5) +
+  facet_wrap(~condition)
+ggsave("../figures/ogt_bycondition.png", width=6, height=2.8)
 
